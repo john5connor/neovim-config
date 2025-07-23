46a5f134c2c483446d0def68b7b5f8e2d8301c5d
John McCarty <mccartyccm120@gmail.com>
2025-07-22 21:41:28 -0400

    basic lsp configuration for C, still need to get autocomplete working and determine better method for including more languages

--- lua/cdizzy/lazy.lua
+++ lua/cdizzy/lazy.lua
@@ -38,9 +38,48 @@ local plugins = {
   'theprimeagen/harpoon',
   'mbbill/undotree',
   'tpope/vim-fugitive',
-  'neovim/nvim-lspconfig',
-  'hrsh7th/nvim-cmp',
-  'hrsh7th/cmp-nvim-lsp',
+  {
+    'neovim/nvim-lspconfig',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-nvim-lsp',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+    },
+    config = function()
+      local lspconfig = require('lspconfig')
+      local cmp = require('cmp')
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            ["<Tab>"] = cmp.mapping.select_next_item(),
+            ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          }),
+          sources = {
+            { name = "nvim_lsp" },
+            { name = "luasnip" },
+          },
+        })
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+
+      lspconfig.clangd.setup({
+        cmd = {'/lib/llvm-18/bin/clangd', '--background-index', '--clang-tidy'},
+        init_options ={
+          fallbackFlags = { '-std=c++17' },
+        },
+      })
+    end,
+  }
 }
 
 local opts = {}
